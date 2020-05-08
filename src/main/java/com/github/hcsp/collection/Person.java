690bef3910aa1dfe4357cd50259d87487efc4c8e
DobbyKim <1175088275@qq.com>
2020-05-08 21:28:12 +0800

    fix hashcode-bug

--- src/main/java/com/github/hcsp/collection/Person.java
+++ src/main/java/com/github/hcsp/collection/Person.java
@@ -55,14 +55,6 @@ public class Person {
         return id.equals(person.id);
     }
 
-    /*@Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + age;
-        return result;
-    }*/
-
     @Override
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
