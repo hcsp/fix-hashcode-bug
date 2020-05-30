4c0a7e79eab1d50c6fdc0b269e48aadc34f00ffb
dingw <dingweibing@outlook.jp>
2020-05-30 20:47:20 +0900

    my commit

--- src/main/java/com/github/hcsp/collection/Person.java
+++ src/main/java/com/github/hcsp/collection/Person.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.collection;
 
+import java.util.Objects;
+
 public class Person {
     /** 身份证号 */
     private final String id;
@@ -42,16 +44,14 @@ public class Person {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-
         Person person = (Person) o;
-        return id.equals(person.id);
+        return age == person.age &&
+                Objects.equals(id, person.id) &&
+                Objects.equals(name, person.name);
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + (name != null ? name.hashCode() : 0);
-        result = 31 * result + age;
-        return result;
+        return Objects.hash(id, name, age);
     }
 }
